1c0dd23ec7d198cc365da518eeab468e7cfe47a1
Julien-Paillassa <julien.paillassa@gmail.com>
2024-09-23 13:09:16 +0200

    fix: fix publishingModule

--- src/modules/publishing/publishing.module.ts
+++ src/modules/publishing/publishing.module.ts
@@ -1,21 +1,13 @@
-import { Module } from '@nestjs/common'
+import { forwardRef, Module } from '@nestjs/common'
 import { PublishingService } from './publishing.service'
 import { PublishingController } from './publishing.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Publishing } from './entities/publishing.entity'
 import { OrchestratorModule } from '../orchestrator/orchestrator.module'
-import { AuthorModule } from '../author/author.module'
-import { BookModule } from '../book/book.module'
-import { CoverModule } from '../cover/cover.module'
-import { FormatModule } from '../format/format.module'
 
 @Module({
   imports: [TypeOrmModule.forFeature([Publishing]),
-    OrchestratorModule,
-    AuthorModule,
-    BookModule,
-    CoverModule,
-    FormatModule
+    forwardRef(() => OrchestratorModule)
   ],
   controllers: [PublishingController],
   providers: [PublishingService],
