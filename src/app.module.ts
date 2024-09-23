7a993e49dbbb6faf6a5f05beb479e01273ae19f5
Julien-Paillassa <julien.paillassa@gmail.com>
2024-09-23 23:34:04 +0200

    fix: set variable env 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -64,9 +64,9 @@ import { CoreModule } from './core.module'
 
 if (process.env.NODE_ENV === 'dev') {
   // dotenv.config({ path: './.env.dev' })
-  dotenv.config({ path: './.env.test' })
+  dotenv.config({ path: './.env' })
 } else if (process.env.NODE_ENV === 'prod') {
-  dotenv.config({ path: './.env.test' })
+  dotenv.config({ path: './.env' })
 }
 
 console.log('DATABASE_HOST', process.env.DATABASE_HOST)
